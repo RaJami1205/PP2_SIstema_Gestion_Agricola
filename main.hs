c82c3271ab0968f6dc398a85dfaf4508841add2e
RaJami1205 <rauljamiralfarorodriguez512@gmail.com>
2025-04-13 16:25:56 -0600

    Inicialización del Proyecto

---
+++ main.hs
@@ -0,0 +1,5 @@
+-- main.hs
+import Menu (mainMenu)  -- Importa solo la función necesaria
+
+main :: IO ()
+main = mainMenu  -- Ejecuta el menú principal
---
+++ src/menu.hs
@@ -0,0 +1,23 @@
+-- src/Menu.hs
+module Menu where
+
+mostrarMenu :: IO ()
+mostrarMenu = do
+    putStrLn "\n"
+    putStrLn "===================================="
+    putStrLn "Bienvenido a Finca Agricola Frucali"
+    putStrLn "===================================="
+    putStrLn "1. Opciones Operativas"
+    putStrLn "2. Opciones Generales"
+    putStrLn "3. Salir"
+    putStr "Ingrese una opción: "
+
+mainMenu :: IO ()
+mainMenu = do
+    mostrarMenu
+    opcion <- getLine
+    case opcion of
+        "1" -> do putStrLn "\nEntrando a Opciones Operativas...\n"; mainMenu
+        "2" -> do putStrLn "\nEntrando a Opciones Generales...\n"; mainMenu
+        "3" -> putStrLn "\nSaliendo del sistema...\n"
+        _   -> do putStrLn "\n¡Opción inválida!\n"; mainMenu
