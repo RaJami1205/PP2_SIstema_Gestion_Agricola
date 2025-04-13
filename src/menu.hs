1d1d8c8a4bb405dee169c857ef333661cd7c3227
RaJami1205 <rauljamiralfarorodriguez512@gmail.com>
2025-04-13 16:30:06 -0600

    Prueba exitosa

--- src/menu.hs
+++ src/menu.hs
@@ -10,7 +10,6 @@ mostrarMenu = do
     putStrLn "1. Opciones Operativas"
     putStrLn "2. Opciones Generales"
     putStrLn "3. Salir"
-    putStrLn "Prueba commit"
     putStr "Ingrese una opción: "
 
 mainMenu :: IO ()
